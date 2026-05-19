a91bf4aa32ce50eb3bd2b15f450f07e4b82f62c4
ianhorn <ian.horn@ky.gov>
2026-05-19 09:38:56 -0400

    create script folder for auxillary file creation - tfws, thumbnails, etc.

--- scripts/post_item_requests.py
+++ scripts/post_item_requests.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 api_url = "https://drwgni8q1h.execute-api.us-west-2.amazonaws.com/"
-collection = "dem-phase3"
+collection = "dem-phase2"
 
 url = f"{api_url}collections/{collection}/items"
 num_workers = 28
